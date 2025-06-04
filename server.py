4aa98e077f68b992c881286d8f74f41be0383677
Chris-Afford <Chris.afford3@gmail.com>
2025-06-05 00:48:33 +0930

    Update and rename Web server.py to server.py

---
+++ server.py
@@ -0,0 +1,19 @@
+# main.py
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from sqlmodel import SQLModel
+from database import engine
+from routes import register_routes
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
+
+register_routes(app)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
