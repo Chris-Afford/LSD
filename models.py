69f6c4be3770f55689701cc07b61f307041871ee
Chris-Afford <Chris.afford3@gmail.com>
2025-06-05 00:49:27 +0930

    Create models.py

---
+++ models.py
@@ -0,0 +1,26 @@
+# models.py
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from pydantic import BaseModel
+
+class Club(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    username: str
+    password_hash: str
+
+class Venue(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    club_id: int
+
+class Result(BaseModel):
+    timestamp: str
+    club_id: int
+    venue_name: str
+    status: str
+    message1: str
+    message2: str
+    track_condition: str
+    correct_weight: str
+    raw_message: str
